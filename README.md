5e515d16d27803752755cbc55235f45228cd6cd7
Khamla <humsoumpholphakdy@hotmail.com>
2023-05-08 14:37:34 -0400

    code done

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # tech-blog
+
+## Description
+
+This is a tech-blog will allow users to input their thoughts into a blog and comment on on other users post.  This app follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.
+
+## Deployment
+
+
+
+## Technologies
+
+* express handlebars
+* MySQL2
+* Sequelize
+* dotenv
+* express-session
+* connect-session-sequelize
+
+## Questions
+Khamla Soumpholphakdy: [Github Page](https://github.com/soumpholphakdy)<br>
+Email: (humsoumpholphakdy@hotmail.com)
---
+++ public/js/login.js
@@ -0,0 +1,31 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const name = document.querySelector('#user-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (name && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ name, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      //console.log(response);
+  
+      if (response.ok) {
+        //window.location.reload(true);
+        //console.log(response);
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
